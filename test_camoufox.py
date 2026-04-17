1e375769eab1f60c34c380b116019282e53bbbda
oo33 <hasnaouiyacine59@gmail.com>
2026-04-17 22:00:26 +0100

    fix: true transparent proxy - no proxy config in camoufox

--- test_camoufox.py
+++ test_camoufox.py
@@ -1,6 +1,6 @@
 from camoufox.sync_api import Camoufox
 
-with Camoufox(headless=True, proxy={"server": "socks5://127.0.0.1:9050"}) as browser:
+with Camoufox(headless=True) as browser:
     page = browser.new_page()
     page.goto("https://check.torproject.org/api/ip")
-    print(page.content())
+    print(page.inner_text("pre"))
